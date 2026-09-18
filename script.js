b3ef5537e8b00d0cd4b33d356964e727765f9008
Stelina <snupsi28@gmail.com>
2026-09-18 10:23:14 +0200

    testim lidhje me supabase

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+async function testoSupabase() {
+    const { data, error } = await supabaseClient
+        .from("availability")
+        .select("*")
+        .limit(1);
+
+    if (error) {
+        console.log("Gabim:", error);
+    } else {
+        console.log("Supabase u lidh me sukses!", data);
+    }
+}
+
+testoSupabase();
