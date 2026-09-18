61a25eb9804f20e14fb64b8ad458cfed7166c93c
Stelina <snupsi28@gmail.com>
2026-09-18 23:23:05 +0200

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -37,7 +37,7 @@ const SUPABASE_URL =
     "https://wzlcpnvymfdhqbamumwp.supabase.co";
 
 const SUPABASE_KEY =
-    "sb_publishable_HpAzGQC7K5N_xvP2QKEUa_4rCHI6zX";
+    "sb_publishable_HpAzGQC7K5N_xvP2QKEUaA_4rCHI6zX";
 
 
 const supabaseClient = window.supabase.createClient(
